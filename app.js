fe1c7df936164930f73e484829f02c9687c29fad
thurstonjm <jeffery.m.thurston@saic.com>
2018-07-13 10:15:12 -0400

    Updates to data

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var app=angular.module('GrillGuide',[]);
 
 app.controller('GuideController',['$scope','$http',function($scope, $http){
-  $scope.demo = "somestring";
+  $scope.demo = "Meat Grilling Guide";
   $http.get("grillguide.json").then(function(response) {
-    $scope.myData = response.data.meats;
+    $scope.meattemp = response.data.meattemps;
+    $scope.myData = response.data.meatsmokes;
     $scope.woodData = response.data.woods;
   });
 }]);
